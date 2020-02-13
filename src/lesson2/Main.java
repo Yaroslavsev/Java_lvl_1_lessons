3ead214beadd9124ef7a3ced558d5ca516a8cf0a
Anton Yaroslavsev <Yaroslavsev@mail.ru>
2020-02-13 17:14:51 +0500

    Домашняя работа 2

---
+++ src/lesson2/Main.java
@@ -0,0 +1,59 @@
+package lesson2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Задание 1
+//        int[] arrConverse = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+//        System.out.println("Изначальный массив - " + Arrays.toString(arrConverse));
+//        for (int i = 0; i < arrConverse.length; i++) {
+//            if (arrConverse[i] == 0) {
+//                arrConverse[i] = 1;
+//            } else {
+//                arrConverse[i] = 0;
+//            }
+//        }
+//        System.out.println("Обратный массив - " + Arrays.toString(arrConverse));
+
+
+        //Задание 2
+//        int[] arrFill = new int[8];
+//        for (int i = 0; i < arrFill.length; i++) {
+//            arrFill[i] = i*3;
+//        }
+//        System.out.println(Arrays.toString(arrFill));
+
+        //Задание 3
+//        int[] arrMultiply = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+//        for (int i = 0; i < arrMultiply.length; i++) {
+//            if (arrMultiply[i] < 6){
+//                arrMultiply[i] *= 2;
+//            }
+//        }
+//        System.out.println(Arrays.toString(arrMultiply));
+
+        //Задание 4
+//        int[][] arrDiagonal = new int[5][5];
+//        for (int i = 0; i < arrDiagonal.length; i++) {
+//            arrDiagonal[i][i] = 1;
+//            for (int j = arrDiagonal.length - 1; j < arrDiagonal.length; j++) {
+//                arrDiagonal[i][j - i] = 1;
+//            }
+//        }
+//
+//        for (int i = 0; i < arrDiagonal.length; i++) {
+//            for (int j = 0; j < arrDiagonal[i].length; j++) {
+//                System.out.printf("%d ", arrDiagonal[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+
+    }
+}
+
+
+
